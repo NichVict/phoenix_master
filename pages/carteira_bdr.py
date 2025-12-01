57644e7eaa8456fb66d57b121145e5b759ed6caf
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-12-01 18:29:48 +0000

    Update carteira_bdr.py

--- pages/carteira_bdr.py
+++ pages/carteira_bdr.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from auth.token_login import require_token, require_carteira
 
-# 🔐 Autenticação + permissão
-user = require_token()
+
 require_carteira("Carteira de BDRs")
 
