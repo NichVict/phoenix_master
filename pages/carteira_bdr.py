4eb83dff6e0a789bd561a51c6b2f8b7f2a378a62
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-12-01 18:59:55 +0000

    Update carteira_bdr.py

--- pages/carteira_bdr.py
+++ pages/carteira_bdr.py
@@ -1,10 +1,3 @@
 import streamlit as st
-from auth.token_login import require_token, require_carteira
 
-# 1) Carrega o usuário pelo token da URL
-user = require_token()
-
-
-
-require_carteira("Carteira de BDRs")
 
