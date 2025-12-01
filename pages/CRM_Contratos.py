4f4f9b9d4700471160a7a24df5293e2dde648543
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-12-01 10:07:27 +0000

    Delete pages/CRM_Contratos.py

--- pages/CRM_Contratos.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("📜 CRM – Contratos")
-
-try:
-    with open("crm/contrato_Aurinvest.pdf", "rb") as f:
-        st.download_button("Baixar Contrato Aurinvest", f, file_name="contrato_Aurinvest.pdf")
-except:
-    st.warning("Contrato não encontrado no diretório crm/.")
