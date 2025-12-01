cdec6c1bfc458c9d4c16d1ec66864b450240c104
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-12-01 19:00:06 +0000

    Update carteira_ibov.py

--- pages/carteira_ibov.py
+++ pages/carteira_ibov.py
@@ -1,10 +1,2 @@
 import streamlit as st
-from auth.token_login import require_token, require_carteira
 
-# 1) Carrega o usuário pelo token da URL
-user = require_token()
-
-# 2) Verifica permissão da carteira específica
-require_carteira("Carteira de Ações IBOV")
-
-# ---- DAQUI PRA BAIXO É O CÓDIGO NORMAL DA PÁGINA ----
