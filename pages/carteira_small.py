749478a635804a724f1e0c81b9e7f690b438f276
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-12-01 14:14:20 +0000

    Update carteira_small.py

--- pages/carteira_small.py
+++ pages/carteira_small.py
@@ -1,1 +1,7 @@
+import streamlit as st
+from auth.token_login import require_token, require_carteira
+
+# 🔐 Autenticação + permissão
+user = require_token()
+require_carteira("Carteira de Small Caps")
 
